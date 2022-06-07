8a4c9b305d3a651c83d51c1e4b3bf27e17322a11
Grushenka <grushenka@hiandd.com>
2022-06-07 10:48:58 +0900

    feat: splash screen

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,9 +1,13 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_collection/routes/navigation_service.dart';
+import 'package:flutter_collection/routes/routes.dart';
 import 'package:flutter_collection/utils/my_colors.dart';
 import 'package:flutter_collection/widgets/custom_scaffold.dart';
 import 'package:flutter_collection/widgets/helper.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:get_it/get_it.dart';
 import 'package:lottie/lottie.dart';
 
 
@@ -16,39 +20,44 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  final router = GetIt.I<NavigationService>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(
+      const Duration(seconds: 3), () => router.navigate(RouteNames.Main)
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
       child: Container(
         padding: EdgeInsets.only(top: Helper.height * .2),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            // Text(
-            //   'Flutter Collection',
-            //   textAlign: TextAlign.center,
-            //   style: Helper.theme.headline2,
-            // ),
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.center,
-            //   children: [
-            //     Text(
-            //       'by grusha ',
-            //       style: Helper.theme.headline2,
-            //     ),
-            //     SvgPicture.asset(
-            //       'assets/default/ic_outlined_heart.svg', 
-            //       height: 30,
-            //       color: MyColors.pastelPink,
-            //       ),
-            //   ],
-            // ),
-            typingFirst(),
-            typingSecond(),
-            const SizedBox(height: 50),
+            colorizedText('FLUTTER'),
+            colorizedText('COLLECTION'),
+            Container(
+              padding: const EdgeInsets.only(top: 30),
+              child: Text(
+                'by Grusha K.',
+                style: Helper.theme.headline1?.copyWith(fontSize: 18)),
+            ),
             Container(
               padding: const EdgeInsets.symmetric(horizontal: 50),
               child: Lottie.asset('assets/splash.json',),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 30),
+              child: Text(
+                'Updated on June 07, 2022',
+                style: Helper.theme.bodyText1,
+              ),
             ),
           ],
         ),
@@ -56,31 +65,25 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 
-  typingFirst() => Center(
-    child: DefaultTextStyle(
-      style: Helper.theme.headline2!,
-      child: AnimatedTextKit(
-          displayFullTextOnTap: true,
+  colorizedText(String text) => AnimatedTextKit(
         animatedTexts: [
-          TypewriterAnimatedText(
-            'FLUTTER COLLECTION',
-          ),
+          ColorizeAnimatedText(
+            text,
+            speed: const Duration(milliseconds: 1000),
+            textStyle: Helper.theme.headline1!.copyWith(
+              fontSize: 36,
+            ),
+            colors: [
+              MyColors.mainScarlet,
+              Colors.orange,
+              Colors.yellow,
+              Colors.green,
+              Colors.blue,
+              Colors.blue.shade900,
+              MyColors.mainPurple
+            ],
+          ), 
         ],
-      ),
-    ),
-  );
-
-  // typingSecond() => Center(
-  //   child: DefaultTextStyle(
-  //     style: Helper.theme.headline2!,
-  //     child: AnimatedTextKit(
-  //       displayFullTextOnTap: true,
-  //       animatedTexts: [
-  //         TypewriterAnimatedText(
-  //           'BY GRUSHA',
-  //         ),
-  //       ],
-  //     ),
-  //   ),
-  // );
+        isRepeatingAnimation: true,
+      );
 }
