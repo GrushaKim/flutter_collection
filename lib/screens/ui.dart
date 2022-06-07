72207808deae4227c8d4b39fb7d564e2c48221c6
Grushenka <grushenka@hiandd.com>
2022-06-07 18:58:10 +0900

    feat: header

--- lib/screens/ui.dart
+++ lib/screens/ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_collection/widgets/header.dart';
 
 class UIScreen extends StatefulWidget {
   UIScreen({Key? key}) : super(key: key);
@@ -10,8 +11,18 @@ class UIScreen extends StatefulWidget {
 class _UIScreenState extends State<UIScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('UI')
+    return Column(
+      children: [
+        Header(
+          title: 'UI Components',
+          canBack: true,
+        ),
+        slotMachine(),
+      ],
     );
   }
+
+  slotMachine() => Container(
+
+  );
 }
--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,22 +1,62 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_collection/routes/navigation_service.dart';
+import 'package:flutter_collection/utils/my_colors.dart';
+import 'package:flutter_collection/widgets/helper.dart';
+import 'package:get_it/get_it.dart';
 
-class Header extends StatefulWidget {
+class Header extends StatelessWidget {
   Header({
-    required this.title,
-    this.canBack,
+    this.title,
+    this.canBack = false,
+    this.onTap,
   });
 
-  final String title;
-  final bool? canBack;
+  final String? title;
+  final bool canBack;
+  final VoidCallback?  onTap;
 
-  @override
-  State<Header> createState() => _HeaderState();
-}
+  final appRouter = GetIt.I<NavigationService>();
 
-class _HeaderState extends State<Header> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: Helper.height * .07,
+      color: MyColors.mainScarlet,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          canBack 
+          ? GestureDetector(
+            onTap: onTap ?? () => appRouter.pop(),
+            child: Container(
+              padding: const EdgeInsets.only(left: 25),
+              child: const Icon(
+                Icons.arrow_back, 
+                size: 24,
+                color: Colors.white,
+              ),
+            ),
+          )
+          : Container(),
+          title != '' && title != null 
+          ? Expanded(
+            child: Container(
+              padding: canBack ? const EdgeInsets.only(right: 50) : EdgeInsets.zero,
+              alignment: Alignment.center,
+              child: Text(
+                title!,
+                style: Helper.theme.bodyText1?.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          )
+          : Container(),
+          Container(),
+        ],
+      ),
     );
   }
 }
